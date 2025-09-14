8227cfc848dbbcfaca893c35dd6512518ffdcb46
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-09-14 11:16:50 -0600

    arreglando comando.sh

--- ALU_vcf/comando.sh
+++ ALU_vcf/comando.sh
@@ -8,4 +8,4 @@ rm -rfv `ls | grep -v ".*\.sv\|.*\.sh\|.*\.tcl"`
 # --- Ejecutar Synopsys Formal con tu TCL ---
 # Ajusta la ruta de vfc si no está en el PATH
 #vfc -tcl run_adder.tcl -full64 -cov all -log vfc_log.txt;
-vc_static -file run_adder.tcl | tee vfc_log.txt
+vcf_shell -file run_adder.tcl | tee vfc_log.txt
