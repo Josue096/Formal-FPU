6c38eb843fc3f1eead0d71d605d4cefd5a161d94
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-10-12 12:07:37 -0600

    empezar mul

--- ALU_vcf/mul/adder_bind.sv
+++ ALU_vcf/mul/adder_bind.sv
@@ -1,13 +1,15 @@
 bind fp_mul fp_mul_checker chk (
 //señales output-input 
-  .r_mode   (r_mode),
-  .fp_X     (fp_X),
-  .fp_Y     (fp_Y),
-  .fp_Z     (fp_Z),
-  .ovrf     (ovrf),
-  .udrf     (udrf)
+  .r_mode     (r_mode),
+  .fp_X       (fp_X),
+  .fp_Y       (fp_Y),
+  .fp_Z       (fp_Z),
+  .ovrf       (ovrf),
+  .udrf       (udrf),
 //señales internas
-
+  .frc_Z_full (fp_mul.frc_Z_full),
+  .frc_X      (fp_mul.frc_X), 
+  .frc_Y      (fp_mul.frc_Y)
 
 );
 
--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -1,9 +1,19 @@
 module fp_mul_checker (
-    input logic [31:0] fp_X,
-    input logic [31:0] fp_Y,
-    input logic [31:0] fp_Z,
-    input logic [2:0] r_mode,
-    input logic       ovrf, 
-    input logic       udrf
+    input logic [31:0]  fp_X,
+    input logic [31:0]  fp_Y,
+    input logic [31:0]  fp_Z,
+    input logic [2:0]   r_mode,
+    input logic         ovrf, 
+    input logic         udrf,
+    //internas
+    input logic [47:0]  frc_Z_full,
+    input logic [22:0]  frc_X, frc_Y
 );
+
+always_comb begin
+// MUL
+MUL_IMPLICITO: assert (((|fp_X[30:23] == 0) || (|fp_Y[30:23] == 0)) ->
+                (fp_Z[30:0] == 31'b0));
+end
+
 endmodule
