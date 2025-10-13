86cfbd1a180bc6be9815f5c2cb697254132f0ca6
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-10-12 20:46:47 -0600

    probando mul

--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -28,13 +28,13 @@ always_comb begin
 
     // MUN
     // Dice que los numeros subnormales
-    MUL_SUB_SON_ZERO: assert (((Xsub || Ysub) && (Xnif == 0) && (Ynif == 0)) ->
+    MUL_SUB_SON_ZERO: assert ((((Xsub && !Ynif) || (Ysub && !Xnif))) ->
                     (fp_Z[30:0] == 31'b0));
     // Dice que los numeros subnormales
     MUL_SUB_POR_SUB: assert (((Xsub && Ysub)) ->
                     (fp_Z[30:0] == 31'b0));
 
-    MUL_ZERO_POR_NUM: assert (((XZero || YZero) && !Xnif && !Ynif) ->
+    MUL_ZERO_POR_NUM: assert (((XZero || YZero) && (Xnif == 0) && (Ynif == 0)) ->
                     (fp_Z[30:0] == 31'b0));
 end
 
