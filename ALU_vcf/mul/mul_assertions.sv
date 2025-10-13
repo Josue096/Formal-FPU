5935ace9ea6c24ce717c6d66dde35c19f345cc60
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-10-12 19:47:21 -0600

    nada pasa

--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -27,8 +27,8 @@ always_comb begin
     MUL_SUB_POR_SUB: assert (((Xsub && Ysub)) ->
                     (fp_Z[30:0] == 31'b0));
 
-    MUL_ZERO_POR_NUM: assert (((XZero ^ YZero) && !Xnif && !Ynif) ->
-                    (fp_Z[30:0] == 31'b0));
+    MUL_ZERO_POR_NUM: assert (((fp_X == 32'h80000000 && fp_Y == 32'h80000000)) ->
+                    (fp_Z == 32'h00000000));
 end
 
 endmodule
