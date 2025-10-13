94dfa0ebbfee3216326cf5953971088e8ce3571f
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-10-12 19:18:34 -0600

    sub siempre es cero

--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -26,6 +26,9 @@ always_comb begin
     // Dice que los numeros subnormales
     MUL_SUB_POR_SUB: assert (((Xsub && Ysub)) ->
                     (fp_Z[30:0] == 31'b0));
+
+    MUL_ZERO_POR_NUM: assert (((XZero || YZero) && !Xnif && !Ynif) ->
+                    (fp_Z[30:0] == 31'b0));
 end
 
 endmodule
