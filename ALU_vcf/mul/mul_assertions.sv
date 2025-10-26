ed03141d55cd14105af0b474cbbe8018ac1ec1f5
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-10-26 17:54:55 -0600

    exp

--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -148,7 +148,7 @@ module fp_mul_checker (
         ROUND_CARRY: assert ((norm_r) -> (carry [23]));
 
         bias = (norm_n||norm_r) ? 8'b01111111 : 8'b01111110; 
-        EXP_NORM: assert (exp_Z == fp_X[31:23]+fp_Y[31:23]-bias);
+        EXP_NORM: assert (exp_Z == fp_X[30:23]+fp_Y[30:23]-bias);
 
         Z_PRUEBA: assert ((fp_X == 32'h40400000 && fp_Y == 32'h40400000 && r_mode == 3'b001) ->
                           (fp_Z == 32'h41100000));
