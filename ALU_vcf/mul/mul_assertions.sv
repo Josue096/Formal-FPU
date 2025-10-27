343dcfafc92a494196861c3f04ef967220696ec1
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-10-26 20:30:18 -0600

    align

--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -170,7 +170,9 @@ module fp_mul_checker (
                         || ((|fp_Y[22:0]) && &fp_Y[30:23])));
 
         Z_PRUEBA: assert ((fp_X == 32'h40400000 && fp_Y == 32'h40400000 && r_mode == 3'b001) ->
-                          (fp_Z == 32'h41100000));
+                          (fp_Z == 32'h40C00000));
+        Z_PRUEBA: assert ((fp_X == 32'h00a00000 && fp_Y == 32'h00a00000 && r_mode == 3'b001) ->
+                          (fp_Z == 32'h01200000));
                         
     end
 
