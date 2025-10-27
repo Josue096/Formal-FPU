205c3194a9ec2043613cbc82326249e4e75315ca
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-10-26 18:09:33 -0600

    exp

--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -152,7 +152,7 @@ module fp_mul_checker (
 
         EXP_UDRF: assert (((fp_X[30:23]+fp_Y[30:23]) <= bias) -> udrf);
 
-        EXP_UDRF: assert (((fp_X[30:23]+fp_Y[30:23]) >= bias + 255) -> ovrf);
+        EXP_OVRF: assert (((fp_X[30:23]+fp_Y[30:23]) >= bias + 255) -> ovrf);
 
         Z_PRUEBA: assert ((fp_X == 32'h40400000 && fp_Y == 32'h40400000 && r_mode == 3'b001) ->
                           (fp_Z == 32'h41100000));
