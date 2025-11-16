7647c9f74d03afa01b21aa82acfc11427581fc06
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-15 21:27:04 -0600

    uderflow

--- ALU_vcf/mul/mul_assertions.sv
+++ ALU_vcf/mul/mul_assertions.sv
@@ -203,6 +203,6 @@ module fp_mul_checker (
         Z_PRUEBA: assert ((fp_X == 32'h40400000 && fp_Y == 32'h40400000 && r_mode == 3'b001) ->
                           (fp_Z == 32'h41100000));           
         Z_PRUEBA_Underflow: assert ((fp_X == 32'h20000000 && fp_Y == 32'h1F800000 && r_mode == 3'b001) ->
-                          (fp_Z == 32'h00400000));     
+                          (fp_Z == 32'h00000000));     
     end
 endmodule
