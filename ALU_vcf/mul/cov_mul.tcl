875e4c9dec06c56628cb202d6ac2d408f204f4cc
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-20 17:02:55 -0600

    tcl

--- ALU_vcf/mul/cov_mul.tcl
+++ ALU_vcf/mul/cov_mul.tcl
@@ -2,8 +2,8 @@ set_app_var fml_mode_on true
 set_fml_appmode COV
 
 # Enable SVA property coverage
-set_app_var fml_enable_prop_density_cov_map true
-set_fml_var fml_enable_property_coverage true
+set_app_var fml_cov_tgl_input_port true
+set_fml_var fml_enable_prop_density_cov_map true
 
 set design fp_mul
 
