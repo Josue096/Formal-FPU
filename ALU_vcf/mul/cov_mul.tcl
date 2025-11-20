b7b17249b6da1c416a752e772e24de14b080838d
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-20 16:57:05 -0600

    tcl

--- ALU_vcf/mul/cov_mul.tcl
+++ ALU_vcf/mul/cov_mul.tcl
@@ -1,9 +1,9 @@
 set_app_var fml_mode_on true
-set_fml_appmode COV 
+set_fml_appmode COV
 
-# Ask VC Formal to read in the filelist
 set design fp_mul
-read_file -top $design -format sverilog -aep all -cov all -sva -vcs {    
+
+read_file -top $design -format sverilog -cov all -sva -vcs {
     ../../FPU/multiplicador/fp_mul.sv
 
     mul_assertions.sv
@@ -13,3 +13,7 @@ read_file -top $design -format sverilog -aep all -cov all -sva -vcs {
 # Ask VC Formal to bring the design out of reset
 sim_run -stable
 sim_save_reset
+
+# Let AEP do its thing
+check_fv -block
+report_fv -list > aep_results.txt
