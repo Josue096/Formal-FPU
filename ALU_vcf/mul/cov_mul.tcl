7043c0fad7d95cf0ddc6d6c2843c7b8ffa1b8fa5
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-20 17:47:07 -0600

    cov

--- ALU_vcf/mul/cov_mul.tcl
+++ ALU_vcf/mul/cov_mul.tcl
@@ -1,9 +1,6 @@
-set_app_var fml_mode_on true
-set_fml_appmode FPV
-set_app_var fml_cov_tgl_input_port true
-set_fml_var fml_enable_prop_density_cov_map true
-
 set design fp_mul
+
+set_app_var fml_mode_on true
 
 read_file -top $design -format sverilog -cov all -sva -vcs {
     ../../FPU/multiplicador/fp_mul.sv
@@ -13,3 +10,24 @@ read_file -top $design -format sverilog -cov all -sva -vcs {
 
 sim_run -stable
 sim_save_reset
+
+check_fv -block
+
+report_fv -list > aep_results_mul.txt
+
+# ----- COBERTURA DISPONIBLE EN TU VCF -----
+
+# Dead code coverage (muy útil)
+compute_dead_code
+
+# Per-property bounded coverage
+compute_per_prop_bounded_cov
+
+# Exportar resultados al Coverage Database
+save_covdb -o cov_results
+
+# Exportar resultados de bounded coverage
+save_per_prop_bounded_cov_results -o bounded_results
+
+# Exportar formal core coverage
+save_formal_core_results -o core_results
