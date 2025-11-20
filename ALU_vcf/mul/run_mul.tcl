6960c8bd49b2061893b4e432b60b585ad2ed90b0
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-20 17:28:56 -0600

    fpv

--- ALU_vcf/mul/run_mul.tcl
+++ ALU_vcf/mul/run_mul.tcl
@@ -2,11 +2,13 @@ set design fp_mul
 
 set_app_var fml_mode_on true
 set_app_var fml_cov_tgl_input_port true
-set_fml_var fml_enable_prop_density_cov_map true
-set_fml_var fml_cov_enable_all true
+
 set_fml_var fml_enable_cov true
-set_fml_var fml_cov_full_signal true
-set_fml_var fml_cover_point_backend_verdi true
+set_fml_var fml_enable_toggle_cov true
+set_fml_var fml_enable_expr_cov true
+set_fml_var fml_enable_branch_cov true
+set_fml_var fml_enable_sva_cov true
+set_fml_var fml_enable_prop_density_cov_map true
 
 read_file -top $design -format sverilog -cov all -sva -vcs {
     ../../FPU/multiplicador/fp_mul.sv
@@ -23,4 +25,3 @@ report_fv -list > aep_results_mul.txt
 
 report_fml_cov -summary > cov_summary.txt
 report_fml_cov -detail > cov_detail.txt
-report_fml_cov -verdi_dir fml_cov_verdi
