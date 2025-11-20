08521605f3041f17a67b2f81c223b00c043b17d6
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-20 16:21:58 -0600

    tcl

--- ALU_vcf/mul/cov_mul.tcl
+++ ALU_vcf/mul/cov_mul.tcl
@@ -1,24 +1,15 @@
 set_app_var fml_mode_on true
-set_fml_appmode AEP
+set_fml_appmode FPV 
 
 # Ask VC Formal to read in the filelist
 set design fp_mul
 read_file -top $design -format sverilog -aep all -sva -vcs {    
     ../../FPU/multiplicador/fp_mul.sv
 
-
+    mul_assertions.sv
     mul_bind.sv
 }
-
-# Since the formal testbench is embedded within the DUT,
-# we ask the tool to drive the clock and resets
-create_clock clk -period 100
-create_reset rst_clk -high
 
 # Ask VC Formal to bring the design out of reset
 sim_run -stable
 sim_save_reset
-
-# Let AEP do its thing
-check_fv -block
-report_fv -list > aep_results.txt
