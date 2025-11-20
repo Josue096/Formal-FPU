0f9812075ba16b7da9bed3141c6ee1fff7f432b2
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-20 15:38:53 -0600

    tcl

--- ALU_vcf/mul/cov_mul.tcl
+++ ALU_vcf/mul/cov_mul.tcl
@@ -1,7 +1,5 @@
 set_app_var fml_mode_on true
-
-set_app_var fml_cov_tgl_input_port true
-set_fml_var fml_enable_prop_density_cov_map true
+set_fml_appmode COV
 
 set design fp_mul
 
@@ -14,5 +12,3 @@ read_file -top $design -format sverilog -cov all -sva -vcs {
 
 sim_run -stable
 sim_save_reset
-check_fv -block
-report_fv -list > aep_results_mul.txt 
