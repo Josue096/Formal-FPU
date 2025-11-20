5251c0c23900f0a13584978c41e56fedb0fdbf90
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-20 17:01:11 -0600

    tcl

--- ALU_vcf/mul/cov_mul.tcl
+++ ALU_vcf/mul/cov_mul.tcl
@@ -1,15 +1,18 @@
 set_app_var fml_mode_on true
 set_fml_appmode COV
 
+# Enable SVA property coverage
+set_app_var fml_enable_prop_density_cov_map true
+set_fml_var fml_enable_property_coverage true
+
 set design fp_mul
 
 read_file -top $design -format sverilog -cov all -sva -vcs {
     ../../FPU/multiplicador/fp_mul.sv
-
     mul_assertions.sv
     mul_bind.sv
 }
 
-# Ask VC Formal to bring the design out of reset
+# Let VC Formal find a stable reset
 sim_run -stable
 sim_save_reset
