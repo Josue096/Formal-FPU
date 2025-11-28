fab7e488f3f31e733cce09bddac116485f15b3b5
Josue096 <125601873+Josue096@users.noreply.github.com>
2025-11-27 23:16:57 -0600

    pruebas comm

--- ALU_vcf/adder/omm_wrapper.sv
+++ ALU_vcf/adder/omm_wrapper.sv
@@ -24,7 +24,9 @@ module fp_comm_wrapper;
   );
 
     always_comb begin
-        COMM: assert (result_ab == result_ba);
+        COMM_SIGNO: assert (result_ab[31] == result_ba[31]);
+        COMM_EXPONENTE: assert (result_ab[30:23] == result_ba[30:23]);
+        COMM_MANTISSA: assert (result_ab[22:0] == result_ba[22:0]);
         COMM_OV: assert (ov1 == ov2);
         COMM_UD: assert (ud1 == ud2);
     end
